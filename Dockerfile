486c55da4b4131afd83592c95167cd626e98b3b7
anjankow <anjankow@protonmail.com>
2024-10-12 14:36:54 +0000

    use rust wrapper for inotify instead

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM rust:1.81.0-slim-bookworm
 
-RUN apt update && apt install -y clang-15 make
+# needed only to generate C bindings
+# - already provided by inotify wrapper
+# RUN apt update && apt install -y clang-15 make
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use inotify::{Inotify, WatchMask};
+
 fn main() {
-    println!("Hello, world!");
+    let mut inotify = Inotify::init().expect("Error while initializing inotify instance");
+
+    // Watch for modify and close events.
+    inotify
+        .watches()
+        .add(
+            "/tmp/inotify-rs-test-file",
+            WatchMask::MODIFY | WatchMask::CLOSE,
+        )
+        .expect("Failed to add file watch");
+
+    // Read events that were added with `Watches::add` above.
+    let mut buffer = [0; 1024];
+    let events = inotify
+        .read_events_blocking(&mut buffer)
+        .expect("Error while reading events");
+
+    for event in events {
+        // Handle event
+    }
 }
