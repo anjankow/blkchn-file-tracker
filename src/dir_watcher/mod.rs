ad092ab25664e1449056d782b2a5c16464f01c9e
anjankow <anjankow@protonmail.com>
2024-10-14 10:34:59 +0000

    parameterize watched events

--- src/dir_watcher/mod.rs
+++ src/dir_watcher/mod.rs
@@ -86,8 +86,10 @@ pub struct Error {
 }
 
 impl Error {
-    pub fn new(message: String) -> Error {
-        Error { message }
+    pub fn new(message: &str) -> Error {
+        Error {
+            message: message.to_string(),
+        }
     }
 }
 
@@ -106,7 +108,7 @@ impl Display for Error {
 }
 
 impl EventType {
-    fn from_mask(mask: inotify::EventMask) -> Option<Vec<EventType>> {
+    fn from_event_mask(mask: inotify::EventMask) -> Option<Vec<EventType>> {
         let mut ret = Vec::with_capacity(1);
         if mask.contains(inotify::EventMask::ATTRIB) {
             ret.push(EventType::AttributeChanged);
@@ -135,34 +137,64 @@ impl EventType {
         }
         None
     }
+
+    fn to_watch_mask(&self) -> inotify::WatchMask {
+        match self {
+            EventType::AttributeChanged => inotify::WatchMask::ATTRIB,
+            EventType::Created => inotify::WatchMask::CREATE,
+            EventType::Deleted => inotify::WatchMask::DELETE,
+            EventType::MovedFrom => inotify::WatchMask::MOVED_FROM,
+            EventType::MovedTo => inotify::WatchMask::MOVED_TO,
+            EventType::Opened => inotify::WatchMask::OPEN,
+            EventType::Written => inotify::WatchMask::CLOSE_WRITE,
+        }
+    }
+}
+
+fn event_types_to_watch_mask(event_types: Vec<EventType>) -> inotify::WatchMask {
+    let mut ret = inotify::WatchMask::empty();
+    for et in event_types {
+        ret |= et.to_watch_mask();
+    }
+    ret
 }
 
 pub struct DirWatcher {
     inotify: inotify::Inotify,
+    dir: String,
 }
 
 impl DirWatcher {
-    pub fn new(directory: &str) -> Result<DirWatcher, Error> {
-        let inotify = inotify::Inotify::init()?;
+    pub fn new(directory: &str, event_types: Vec<EventType>) -> Result<DirWatcher, Error> {
+        let watch_mask = event_types_to_watch_mask(event_types);
+        if watch_mask.is_empty() {
+            return Err(Error::new("No known event types found in event_types"));
+        }
 
-        let event_mask = inotify::WatchMask::ATTRIB
-            | inotify::WatchMask::CLOSE_WRITE
-            | inotify::WatchMask::CREATE
-            | inotify::WatchMask::DELETE
-            | inotify::WatchMask::MOVE
-            | inotify::WatchMask::OPEN;
+        let inotify = inotify::Inotify::init()?;
 
         inotify
             .watches()
-            .add(directory, event_mask)?;
+            .add(directory, watch_mask)?;
 
-        Ok(DirWatcher { inotify })
+        Ok(DirWatcher {
+            inotify,
+            dir: directory.to_string(),
+        })
     }
 
     pub fn run_blocking(&mut self, tx: mpsc::Sender<Event>) -> Result<(), Error> {
         loop {
-            let events = self.read_events()?;
+            // Read events from inotify
+            let mut buffer = [0; 1024];
+            let events = self
+                .inotify
+                .read_events_blocking(&mut buffer)?;
 
+            // Extract them and enrich with file metadata
+            let events = self.extract_events(events)?;
+
+            // Send events to the listener
             for event in events {
                 let _ = tx
                     .send(event.clone())
@@ -181,76 +213,68 @@ impl DirWatcher {
         }
     }
 
-    fn read_events(&mut self) -> Result<Vec<Event>, Error> {
-        let mut buffer = [0; 1024];
-        let events = self
-            .inotify
-            .read_events_blocking(&mut buffer)?;
+    // Inotify event is a mask, which means that potentially more events
+    // are encoded within one inotify::Event. We want to create a separate
+    // event for each of them. Stat of a file should be checked just once for each file
+    // every time when `inotify.read_events_blocking` returns,
+    // so every time when this function is called.
+    fn extract_events(&self, inotify_events: inotify::Events) -> Result<Vec<Event>, Error> {
+        let mut file_infos: HashMap<String, FileInfo> = HashMap::new();
+        let mut ret_events = Vec::new();
 
-        extract_events(events)
-    }
-}
+        for ie in inotify_events {
+            let file_name = match ie.name {
+                // We care only about the events with associated file names
+                None => continue,
+                Some(n) => n
+                    .to_string_lossy()
+                    .to_string(),
+            };
 
-// Inotify event is a mask, which means that potentially more events
-// are encoded within one inotify::Event. We want to create a separate
-// event for each of them. Stat of a file should be checked just once for each file
-// every time when `inotify.read_events_blocking` returns,
-// so every time when this function is called.
-fn extract_events(inotify_events: inotify::Events) -> Result<Vec<Event>, Error> {
-    let mut file_infos: HashMap<String, FileInfo> = HashMap::new();
-    let mut ret_events = Vec::new();
+            let event_types = match EventType::from_event_mask(ie.mask) {
+                // No known events found
+                None => continue,
+                Some(et) => et,
+            };
 
-    for ie in inotify_events {
-        let file_name = match ie.name {
-            // We care only about the events with associated file names
-            None => continue,
-            Some(n) => n
-                .to_string_lossy()
-                .to_string(),
-        };
+            // Now we get the file metadata, if not present in the map.
+            let file_info = match file_infos.get(&file_name) {
+                Some(fi) => Some(fi.clone()),
+                None => match self.read_file_metadata(&file_name) {
+                    Err(e) => {
+                        println!("Failed to read file info of a file {}: {}", file_name, e);
+                        None
+                    }
+                    Ok(fi) => {
+                        // We want to store it in our map, maybe there are more events
+                        // associated with this file in the input events.
+                        file_infos.insert(file_name.clone(), fi.clone());
+                        Some(fi)
+                    }
+                },
+            };
 
-        let event_types = match EventType::from_mask(ie.mask) {
-            // No known events found
-            None => continue,
-            Some(et) => et,
-        };
-
-        // Now we get the file metadata, if not present in the map.
-        let file_info = match file_infos.get(&file_name) {
-            Some(fi) => Some(fi.clone()),
-            None => match read_file_metadata(&file_name) {
-                Err(e) => {
-                    println!("Failed to read file info of a file {}: {}", file_name, e);
-                    None
-                }
-                Ok(fi) => {
-                    // We want to store it in our map, maybe there are more events
-                    // associated with this file in the input events.
-                    file_infos.insert(file_name.clone(), fi.clone());
-                    Some(fi)
-                }
-            },
-        };
-
-        for event_type in event_types {
-            ret_events.push(Event {
-                event_type: event_type,
-                file_name: file_name.clone(),
-                file_info: file_info.clone(),
-            });
+            for event_type in event_types {
+                ret_events.push(Event {
+                    event_type: event_type,
+                    file_name: file_name.clone(),
+                    file_info: file_info.clone(),
+                });
+            }
         }
+        Ok(ret_events)
     }
-    Ok(ret_events)
-}
 
-fn read_file_metadata(file_name: &str) -> Result<FileInfo, Error> {
-    let metadata = fs::metadata(&file_name)?;
+    fn read_file_metadata(&self, file_name: &str) -> Result<FileInfo, Error> {
+        let path = std::path::Path::new(self.dir.as_str()).join(file_name);
+        let metadata = fs::metadata(path)?;
 
-    Ok(FileInfo {
-        size: metadata.len(),
-        mode: metadata.permissions().mode(),
-        access_ts: metadata.accessed().ok(),
-        modify_ts: metadata.modified().ok(),
-        created_ts: metadata.created().ok(),
-    })
+        Ok(FileInfo {
+            size: metadata.len(),
+            mode: metadata.permissions().mode(),
+            access_ts: metadata.accessed().ok(),
+            modify_ts: metadata.modified().ok(),
+            created_ts: metadata.created().ok(),
+        })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ mod dir_watcher;
 fn main() {
     let dir = "./c"; // path relative to Cargo.toml
     let (tx, rx) = std::sync::mpsc::channel();
-    dir_watcher::DirWatcher::new(dir)
+    let event_types = vec![
+        dir_watcher::EventType::AttributeChanged,
+        dir_watcher::EventType::Created,
+        dir_watcher::EventType::Deleted,
+        dir_watcher::EventType::MovedFrom,
+        dir_watcher::EventType::MovedTo,
+        dir_watcher::EventType::Written,
+    ];
+    dir_watcher::DirWatcher::new(dir, event_types)
         .unwrap()
         .run_blocking(tx)
         .expect("Should never return");
